299942ee5918d0fa18279018283f9518dbca6217
Ln3242 <109390328+Ln3242@users.noreply.github.com>
2022-10-30 20:46:27 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -34,7 +34,7 @@ local funcs = {}; do
             return readfile(url)
         end
 
-        local newUrl = (bypass and "https://raw.githubusercontent.com/joeengo/" or "https://raw.githubusercontent.com/joeengo/engoware/main/") .. url:gsub("engoware/", ""):gsub("engoware\\", "")
+        local newUrl = (bypass and "https://raw.githubusercontent.com/Ln3242/" or "https://raw.githubusercontent.com/Ln3242/engoware/main/") .. url:gsub("engoware/", ""):gsub("engoware\\", "")
         local response = request({
             Url = bypass2 and url or newUrl,
             Method = "GET",
